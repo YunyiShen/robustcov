49a05af6ab0128dd57f448735ba200c86ab2cdf2
YunyiShen <yshen99@wisc.edu>
2021-04-07 14:30:59 -0500

    R helpers

---
+++ R/simu_data.R
@@ -0,0 +1,26 @@
+# ' Sample contaminated normal 
+# ' sample normal distribution with normal contamination
+# ' @param n samplesize
+# ' @param Omega precision matrix of the normal 
+# ' @param byrow whether the comtamination happened by row? FALSE stand for cellwise
+# ' @param cont_rate how many cells/rows are contaminated?
+# ' @param mu mean of the contamination
+# ' @param sd standard deviation of the contamination 
+# ' @export
+conta_normal <- function(n, Omega, byrow = FALSE,
+                        cont_rate = 0.05, 
+                        mu = 10, sd = sqrt(0.2)){
+
+    res <- rmvnorm(n, Omega)
+    if(byrow){
+        p <- nrow(Omega)
+        contind <- sample.int(n , size = floor(cont_rate * n))
+        res[contind,] <- rnorm(length(contind)*p, mu, sd)
+    }
+    else{
+        nn <- length(res)
+        contind <- sample.int(nn , size = floor(cont_rate * nn))
+        res[contind] <- rnorm(length(contind), mu, sd)
+    }
+    return(res)
+}
