deb7fa30a5c60e78fda8d257dacaed26c67bec01
Yunyi Shen <34624472+YunyiShen@users.noreply.github.com>
2021-04-13 13:50:14 -0500

    current results

--- simulation/QUIC_simus.R
+++ simulation/QUIC_simus.R
@@ -11,13 +11,15 @@ source("./simulation/cont_norm.R") # contaimanted norm
 
 results_file <- "./simulation/Res/res1.csv"
 
-graphs <- c("omega_banded", "omega_sparse", "omega_dense", "diag")
-covmats <- c(cov, corSpearmanmat,corKendallmat, 
-            corQuadrantmat,covGKmat, 
-            covNPDmat, covOGKmat, covSpearmanUmat)
-covmats_name <- c("cov", "corSpearmanmat","corKendallmat", 
-            "corQuadrantmat","covGKmat", 
-            "covNPDmat", "covOGKmat", "covSpearmanUmat")
+graphs <- c("omega_sparse", "omega_banded", "omega_dense", "diag")
+covmats <- c(cov,corSpearmanmat,corKendallmat, 
+            #corQuadrantmat,covGKmat, 
+            #covNPDmat, 
+            covOGKmat, covSpearmanUmat)
+covmats_name <- c("cov","corSpearmanmat","corKendallmat", 
+            #"corQuadrantmat","covGKmat", 
+            #"covNPDmat", 
+            "covOGKmat", "covSpearmanUmat")
 distributions <- c("rmvnorm","conta_normal_cell_5","conta_normal_cell_10", 
                     "conta_normal_row_5","conta_normal_row_10", "rmvt", 
                     "raltert")
@@ -37,13 +39,20 @@ colnames(res) <- c("cov_measure","p",
 
 i_data <- 1
 # since the algorithms are already parallelized, will do for here in R
-for(i in 1:length(covmats)){
-    for(p in ps){
-        for(g in graph){
-            for(d in distributions){
+
+for(p in ps){
+    for(g in graphs){
+        for(d in distributions){
+            for(i in 1:length(covmats)){
+                cat("covmat:" ,covmats_name[i], " p =",p," g =", g, " d =", d, " rep = CV", "\n")
                 # will do CV here only ones
-                omega <- do.call(g, list(p=p))
-                data1 <- do.call(d, list(n = n, Omega = omega))
+                if(g=="diag") omega <- diag(p)
+                else{
+                    omega <- do.call(g, list(p=p))
+                }
+                dist_para <- list(n = n, Omega = omega)
+                if(d %in% c("rmvt","raltert")) dist_para$nu = 3
+                data1 <- do.call(d, dist_para)
                 QUICres <- tryCatch( robQUIC(data=data1, covest = covmats[[i]], CV = TRUE, msg = 0), 
                                     error =function(e){list()})
                 if(length(QUICres)>0){
@@ -67,7 +76,7 @@ for(i in 1:length(covmats)){
                 write.csv(res,results_file, row.names = FALSE)
                 for(j in 2:rep){
                     cat("covmat:" ,covmats_name[i], " p =",p," g =", g, " d =", d, " rep =",j, "\n")
-                    data1 <- do.call(d, list(n = n, Omega = omega))
+                    data1 <- do.call(d, dist_para)
                     QUICres <- tryCatch( robQUIC(data=data1, covest = covmats[[i]], lambda = lambda,CV = FALSE, msg = 0), 
                                     error =function(e){list()})
                     if(length(QUICres)>0){
